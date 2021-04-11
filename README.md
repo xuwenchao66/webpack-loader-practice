12c44f8557197870aef5fdc30729daa2fae3f4d1
xuwenchao <842390367@qq.com>
2021-04-11 23:17:03 +0800

    add schema validate & readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# webpack-loader-practice
+
+博客 [webpack loader 开发入门](https://xuwenchao.site/blogs/webpack-write-a-loader.html) 相关实践代码。
--- src/getOptions.js
+++ src/getOptions.js
@@ -1,6 +1,11 @@
 const { getOptions } = require('loader-utils')
+const { validate } = require('schema-utils')
+const schema = require('./schema.json')
 
 module.exports = (ctx) => {
   const options = getOptions(ctx)
+  validate(schema, options, {
+    name: 'webpack replace loader'
+  })
   return options
 }
